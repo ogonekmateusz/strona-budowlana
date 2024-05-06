ed677be9d16ec3433b82bee302f96cc03a1c7d94
DESKTOP-VA58A1P\Mateusz <ogonekmateusz69@gmail.com>
2024-05-06 17:39:03 +0200

    create and import global styles, create header component and add bootsrap libary

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import './style.css'
-import router from './router/index.ts'
 import App from './App.vue'
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle';
 createApp(App).use(router).mount('#app')
+import router from './router/index.ts'
